8e763be8c060a9b0ba19ee9841def3694a0d170d
victoriacacao <victoriacostacacao@gmail.com>
2024-11-21 23:29:00 -0300

    Update models.py
    modelagem de dados completa

--- afpuc/afpuc/models.py
+++ afpuc/afpuc/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models.fields import BooleanField
 
 class Conta(models.Model):
     matricula = models.IntegerField(unique=True)
@@ -7,3 +9,31 @@ class Conta(models.Model):
 
     def __str__(self):
         return self.nome
+
+class usuario(models.Model):
+  nomeCompleto = models.CharField(max_length=100)
+  idPUC = models.EmailField(max_length=100)
+  emailDoCliente = models.EmailField(max_length=100)
+  senha = models.Charfield(max_length=100)
+
+  def __str__(self):
+    return self.nomeCompleto
+
+class itens(models.Model):
+  nome_produto = models.CharField(max_length=300)
+  id_produto = models.ForeignKey(usuario, on_delete=models.CASCADE)
+  valor_produto = models.DecimalField(max_digits=10, decimal_places=2, validadors=[MinValueValidator(0.00)])
+  descricao = models.TextField(max_length=300)
+  foto_produto = models.ImageField(upload_to='produtos')
+
+class itensDoPedido(models.Model):
+  id_pedido = models.ForeignKey(itens, on_delete=models.CASCADE)
+  id_cliente = models.ForeingKey(usuario, on_delete=models.CASCADE)
+  dataHorarioDoPedido = models.DataTimeField()
+  valorTotalDoPedido = models.DecimalField(max_digits=10, decimal_places=2, valid)
+  numDeItens = models.IntegerField(validators=[MinValueValidator(1)])
+  statusDoPedido = models.BooleanField(default=False)
+
+
+
+
