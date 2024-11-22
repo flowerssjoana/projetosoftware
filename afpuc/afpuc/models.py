0d2d6ace1781986860c4ec560c70b6023a2ca8f8
victoriacacao <victoriacostacacao@gmail.com>
2024-11-21 23:36:24 -0300

    Update models.py

--- afpuc/afpuc/models.py
+++ afpuc/afpuc/models.py
@@ -11,29 +11,35 @@ class Conta(models.Model):
         return self.nome
 
 class usuario(models.Model):
-  nomeCompleto = models.CharField(max_length=100)
-  idPUC = models.EmailField(max_length=100)
-  emailDoCliente = models.EmailField(max_length=100)
-  senha = models.Charfield(max_length=100)
+    nomeCompleto = models.CharField(max_length=100)
+    idPUC = models.EmailField(max_length=100)
+    emailDoCliente = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=100)
 
-  def __str__(self):
-    return self.nomeCompleto
+    def __str__(self):
+        return self.nomeCompleto
 
 class itens(models.Model):
-  nome_produto = models.CharField(max_length=300)
-  id_produto = models.ForeignKey(usuario, on_delete=models.CASCADE)
-  valor_produto = models.DecimalField(max_digits=10, decimal_places=2, validadors=[MinValueValidator(0.00)])
-  descricao = models.TextField(max_length=300)
-  foto_produto = models.ImageField(upload_to='produtos')
+    nome_produto = models.CharField(max_length=300)
+    id_produto = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    valor_produto = models.DecimalField(max_digits=10, decimal_places=2, validadors=[MinValueValidator(0.00)])
+    descricao = models.TextField(max_length=300)
+    foto_produto = models.ImageField(upload_to='produtos')
+    
+    def __str__(self):
+        return self.nome_produto
+
 
 class itensDoPedido(models.Model):
-  id_pedido = models.ForeignKey(itens, on_delete=models.CASCADE)
-  id_cliente = models.ForeingKey(usuario, on_delete=models.CASCADE)
-  dataHorarioDoPedido = models.DataTimeField()
-  valorTotalDoPedido = models.DecimalField(max_digits=10, decimal_places=2, valid)
-  numDeItens = models.IntegerField(validators=[MinValueValidator(1)])
-  statusDoPedido = models.BooleanField(default=False)
+    id_pedido = models.ForeignKey(itens, on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    dataHorarioDoPedido = models.DateTimeField()
+    valorTotalDoPedido = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00))
+    numDeItens = models.IntegerField(validadors=[MinValueValidator(1)])
+    statusDoPedido = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"Pedido {self.id_pedido} - Cliente {self.id_cliente}"
 
 
 
